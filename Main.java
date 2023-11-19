8661a6b0c754924d2052f3720121a9d502cf9055
Noureddine Achbili <146972598+N0urDEV@users.noreply.github.com>
2023-11-19 23:47:45 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player("John", 25, 1);
+
+        player.passBall();
+        player.shootBall();
+        player.jump();
+    }
+}
